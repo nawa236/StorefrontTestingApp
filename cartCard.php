136b4a7a88962cb1cc7f788ecf1d494ea1063bd3
nawa236 <nawa236@uky.edu>
2019-11-14 20:41:27 -0500

    cart v1.8

--- cartCard.php
+++ cartCard.php
@@ -4,17 +4,18 @@ class cartCard {
 
 function __construct($name,$sku,$price,$quantity,$pid) {
       echo '<div class="cartcard">';
-      echo "<h1 style=\"float:left;\">$name</h1>";
+      echo '<p style="float:left;">$name  </p>';
       $formattedPrice = number_format ($price,2);
-      echo "<p style=\"float:right;\">SKU: ";
-      echo "$sku";
-	  echo "</p>";
-      echo '<p class=\"price\" style=\"float:right;\">$' . $formattedPrice . '</p>';
-      echo '<p style=\"float:right;\">Quantity: <input type="number" id="cart_quantity_' . $pid; 
-      echo '" style="width: 60px" name="quantity" min="0" onkeypress="return event.charCode >= 48" step="1" value=' . $quantity . '></p>';
-      echo "<p style=\"float:right;\">>Total: $";
+      echo '<p style="float:right;">SKU: ';
+      echo '$sku';
+	  echo '</p>';
+	  echo '<p style="float:right;">Total: $';
       $total = $price * $quantity;
-      echo number_format($total,2)."<p>";
+      echo 'number_format($total,2)."<p>';
+      echo '<p style="float:right;">$' . $formattedPrice . ' </p>';
+      echo '<p style="float:right;">Quantity: <input type="number" id="cart_quantity_' . $pid; 
+      echo '" style="width: 60px" name="quantity" min="0" onkeypress="return event.charCode >= 48" step="1" value=' . $quantity . '> </p>';
+
     echo "</div>";
  }
 
