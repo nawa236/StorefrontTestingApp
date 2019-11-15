27c595b364476c8e1d8da1728859f41db86f2f34
nawa236 <nawa236@uky.edu>
2019-11-14 20:19:59 -0500

    cart v1.6

--- cartCard.php
+++ cartCard.php
@@ -6,7 +6,7 @@ function __construct($name,$sku,$price,$quantity,$pid) {
       echo '<div class="cartcard">';
       echo "<h1 style=\"float:left;\">$name</h1>";
       $formattedPrice = number_format ($price,2);
-      echo "<p style=\"float:left;\"> SKU: " . $sku "</p>";
+      echo "SKU: " . $sku;
       echo '<p class=\"price\" style=\"float:left;\">$' . $formattedPrice . '</p>';
       echo '<p style=\"float:left;\">Quantity: <input type="number" id="cart_quantity_' . $pid; 
       echo '" style="width: 60px" name="quantity" min="0" onkeypress="return event.charCode >= 48" step="1" value=' . $quantity . '></p>';
