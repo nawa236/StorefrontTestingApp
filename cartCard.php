a682b94f27f92c67878bbc7d1e7d49ce416ea476
nawa236 <nawa236@uky.edu>
2019-11-14 20:15:26 -0500

    cart v1.3

--- cartCard.php
+++ cartCard.php
@@ -4,13 +4,13 @@ class cartCard {
 
 function __construct($name,$sku,$price,$quantity,$pid) {
       echo '<div class="cartcard">';
-      echo "<h1>$name</h1>";
+      echo "<h1 style=\"float:left;\">$name</h1>";
       $formattedPrice = number_format ($price,2);
       echo "SKU: " . $sku;
-      echo '<p class=\"price\">$' . $formattedPrice . '</p>';
-      echo 'Quantity: <input type="number" id="cart_quantity_' . $pid; 
-      echo '" style="width: 60px" name="quantity" min="0" onkeypress="return event.charCode >= 48" step="1" value=' . $quantity . '>';
-      echo "<p>Total: $";
+      echo '<p class=\"price\" style=\"float:right;\">$' . $formattedPrice . '</p>';
+      echo '<p style=\"float:right;\">Quantity: <input type="number" id="cart_quantity_' . $pid; 
+      echo '" style="width: 60px" name="quantity" min="0" onkeypress="return event.charCode >= 48" step="1" value=' . $quantity . '></p>';
+      echo "<p style=\"float:right;\">>Total: $";
       $total = $price * $quantity;
       echo number_format($total,2)."<p>";
     echo "</div>";
