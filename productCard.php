74bf6752df6e2ae8700602c41c1e7a076307d214
Memurray <memu225@uky.edu>
2019-11-16 20:30:11 +0000

    Minor formatting changes

--- productCard.php
+++ productCard.php
@@ -7,14 +7,14 @@ function __construct($name,$price,$description,$id) {
       $image = "./images/" . $id . ".jpg"; 
       echo '<div class="card">';
       echo "<img src=$image>";
-      echo "<h1>$name</h1>";
+      echo "<h1 class='name'>$name</h1>";
       $formattedPrice = number_format ($price,2); 
       echo "<p class=\"price\">\$$formattedPrice</p>";
       echo "<p class='desc'>$description</p>";
-      echo '<p><a href="./productPage.php?productID=';
+      echo '<a href="./productPage.php?productID=';
       echo $id; 
       echo '" class="button" ';
-      echo "id=product_button_$id >Configure</a></p>";
+      echo "id=product_button_$id >Configure</a>";
     echo "</div>";
  }
 
