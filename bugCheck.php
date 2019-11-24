a1972019162dc81099ae1ecfc49d535e2542ca2c
Memurray <memu225@uky.edu>
2019-11-24 02:40:06 +0000

    Update bugCheck to support end date

--- bugCheck.php
+++ bugCheck.php
@@ -2,7 +2,7 @@
 function bug_check($bug_id){
   include('dbConnect.php');
   $user_id = $_COOKIE["TriStorefrontUser"];
-  $query = "SELECT * FROM bug,assignment WHERE bug.id=bugid AND bugid = $bug_id AND userid = $user_id;";
+  $query = "SELECT * FROM bug,assignment WHERE bug.id=bugid  AND (edate > now() OR edate is Null) AND bugid = $bug_id AND userid = $user_id;";
   $statement = $connect->prepare($query);
   $statement->execute();
   $result = $statement->fetchAll();
