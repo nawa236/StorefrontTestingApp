3804db9c2b398ea06d36c1351e6875587a14b9b0
Seanna Lea LoBue <seannalea.lobue@gmail.com>
2019-12-03 21:10:35 -0500

    Updated Save Bug feature to provide an alert with the bug ID when save is complete. Updated bug list on the editing  side to display the ID in the drop down list.

--- saveBug.php
+++ saveBug.php
@@ -2,11 +2,12 @@
     include("database.php");
     $db = new Database();
 
-    $name = $_POST["name"];
-    $area = $_POST["functional_area"];
-    $desc = $_POST["description"];
-    $code = $_POST["codeblock"];
+    $name = addslashes($_POST["name"]);
+    $area = addslashes($_POST["functional_area"]);
+    $desc = addslashes($_POST["description"]);
+    $code = addslashes($_POST["codeblock"]);
     $success = -1;
+    $result = array();
 
     $select = "SELECT * FROM bug WHERE name=$name AND functional_area=$area";
     $count = $db->runQuery($select);
@@ -15,10 +16,12 @@
     }else{
         $insert = "INSERT INTO bug (name, functional_area, description, codeblock) VALUES ('$name', '$area', '$desc', '$code')";
         $success = $db->runInsert($insert);
+        $select = "SELECT id, name FROM bug WHERE name='" . $_POST['name'] . "' AND functional_area='" . $_POST['functional_area'] . "';";
+        $result = $db->runDataQuery($select);
     }
 
     if($success > 0){
-        echo "Save to database was successful.";
+        echo json_encode($result);
     }else{
         echo $success . " - " . $insert . "\r\n";
         echo "Error in saving bug to database.";
