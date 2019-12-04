e78c08f3a987410d90ddca9193356ca56dbb9849
Seanna Lea LoBue <seannalea.lobue@gmail.com>
2019-12-03 21:57:54 -0500

    Update adminCookie checking to only check for the authentication type based on the id stored in the cookie.

--- adminCookie.php
+++ adminCookie.php
@@ -1,32 +1,16 @@
 <?php
   include('dbConnect.php');
-  $accountName = "";
   $cookie_name1 = "TriStorefrontUser";
-  $cookie_name2 = "TriStorefrontName";
-  if(!isset($_COOKIE[$cookie_name1]))
+    if(!isset($_COOKIE[$cookie_name1])){
         header("Location: ./login.php");
-
-  // If name is not stored in a cookie, query the name of the active user ID
-  if(!isset($_COOKIE[$cookie_name2])){
-        $query = "SELECT fname, type FROM customer WHERE id=" . $_COOKIE[$cookie_name1] . ";";
+    }else{
+        $query = "SELECT type FROM authentication WHERE id=" . $_COOKIE[$cookie_name1] . ";";
         $statement = $connect->prepare($query);
         $statement->execute();
         $result = $statement->fetchAll();
-        $newName = $result[0]['fname'];
+        $accounttype = $result[0]['type'];
         if($result[0]['type'] != 1){
             header("Location: ./login.php");
         }
-        setcookie("TriStorefrontName", $newName, time()+3600, '/');
-        $accountName = $newName;
-  }
-  else
-        $accountName = $_COOKIE[$cookie_name2];
-
-  $id = $_COOKIE[$cookie_name1];
-  $query = "SELECT * FROM authentication WHERE id=$id;";
-  $statement = $connect->prepare($query);
-  $statement->execute();
-  $result = $statement->fetchAll();
-  $accounttype = 0;
-  $accounttype = $result[0]["type"];
-  ?>
+    }
+?>
