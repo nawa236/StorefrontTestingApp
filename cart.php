794e5ea46c1a15659b87f9a8e52e53fa29f7b970
Memurray <39600016+Memurray@users.noreply.github.com>
2019-12-10 18:48:58 -0500

    Remove unintended addition from demo branch

--- cart.php
+++ cart.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="icon" type="image/png"  href="./images/icon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
